f96bf56f38390b0bd5a98c189cdbe88db3895393
lenhat509 <60248463+lenhat509@users.noreply.github.com>
2020-08-22 12:36:06 -0700

    Update config.py

--- flack/FlackProject/config.py
+++ flack/FlackProject/config.py
@@ -1,7 +1,7 @@
 import json
 
-#import your secret key
-file = open('C:\\Users\\Le Nhat\\VSC project\\flack\\FlackProject\\config.json')
+#create your own config.json file that contain your SECRET_KEY
+file = open('config.json')
 dic = json.load(file)
 class Config:
     SECRET_KEY = dic.get('SECRET_KEY')
